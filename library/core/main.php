e57578bcda73098b4cc055522a5404fb898946c6
cleric <skyclr@gmail.com>
2018-07-09 16:14:53 +0300

    Next

--- library/core/main.php
+++ library/core/main.php
@@ -66,19 +66,5 @@ set_error_handler(function($code, $message, $file, $line) {
 }, E_ALL | E_STRICT);
 
 register_shutdown_function(function() {
-	switch($code) {
-		case E_ERROR    :
-			$type = "php error";
-			break;
-		case E_PARSE    :
-			$type = "php parse";
-			break;
-		case E_STRICT    :
-			$type = "php error";
-			break;
-		default:
-			$type = "php";
-			break;
-	}
 
 });
